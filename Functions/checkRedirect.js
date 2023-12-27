a04b781866e474c07c5750787ccb2322870a41e7
Airbus-A330 <united.vietnam.airlines@gmail.com>
2023-12-26 17:36:09 -0800

    better recursion methods

--- Functions/checkRedirect.js
+++ Functions/checkRedirect.js
@@ -2,19 +2,15 @@ const fetch = (...args) => import('node-fetch').then(({
     default: fetch
 }) => fetch(...args));
 
-let redirectCount = 0;
-
-const checkRedirects = async (url) => {
+const checkRedirects = async (url, count = 0) => {
     let domain = await fetch(url);
     let status = await domain.status;
-    if (status == 301 || status == 302) {
-        redirectCount++;
-        await checkRedirects(url);
+
+    if (!(status == 301 || status == 302)) {
+        return count;
     }
 
-    setTimeout(async () => {
-        return redirectCount;
-    });
-};
+    await checkRedirects(url, count++);
+}
 
 module.exports = checkRedirects;
